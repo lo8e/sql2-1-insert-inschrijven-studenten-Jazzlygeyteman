c6a0dab462899d365cefdb08cf2890b5446f8ad8
Jazzly <jazzlygeyteman1@gmail.com>
2019-05-13 09:35:11 +0200

    opdracht 1

---
+++ inschrijvingen.sql
@@ -0,0 +1,8 @@
+USE `lo8e_sql2`;
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES 
+('lo9e-amo1', 'akr02', 2018, 'amo'),
+ ('lo9e-amo2', 'bkr02', 2018, 'amo'),
+ ('lo9e-amo3', 'rs001', 2018, 'amo'),
+ ('lo9e-amo4', 'vlt03', 2018, 'amo');
+ 
+ 
