cc3dcd7019907c64774c834f10f5d42882a1506a
Jazzly <jazzlygeyteman1@gmail.com>
2019-05-13 09:51:19 +0200

    opdracht 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -6,3 +6,16 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES
  ('lo9e-amo4', 'vlt03', 2018, 'amo');
  
  
+INSERT INTO studenten (ov_nummer, achternaam, 
+tussenvoegsel, voorletters, voornaam, geboortedatum ,postcode ,plaats
+,gewicht,lengte,actief,inschrijvings_datum ) VALUES 
+(99088, 'harmsen', 'h', 'P', 'Piet','1992-05-26 00:00:00', '2211 LK', 'Katwijk', '77.9', 140, 1, '2018-01-01'),
+(99988, 'aarmsen', 'h', 'H', 'Hiet','1992-05-26 00:00:00', '2210 LK', 'Katwijk', '77.9', 140, 1, '2018-01-01'),
+(99987, 'darmsen', 'h', 'D', 'Diet','1992-05-26 00:00:00', '2219 LK', 'Katwijk','77.9', 140, 1, '2018-01-01'),
+(99986, 'marmsen', 'h', 'E', 'Eiet','1992-05-26 00:00:00', '2218 LK','Katwijk', '77.9', 140, 1, '2018-01-01');
+
+
+
+
+
+ 
